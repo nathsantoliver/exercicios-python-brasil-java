56e293f34aa9f98d43fe776e872c47a6b0b0f322
Nathália Oliveira <nathaliaolisantana@gmail.com>
2023-08-14 13:19:57 -0300

    att repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercicios SIte Python Brasil
+
+Resolução dos exercícios do site [pythonbrasil](https://wiki.python.org.br/).
---
+++ src/repeticao/Questao02.java
@@ -0,0 +1,31 @@
+package repeticao;
+
+import java.util.Scanner;
+
+/*
+    Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual
+    ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+*/
+
+public class Questao02 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nomeUsuario;
+        String senha;
+
+        System.out.println("Digite um Nome de Usuário e Senha");
+        System.out.println("Nome de Usuário: ");
+        nomeUsuario = scan.nextLine();
+        System.out.println("Senha: ");
+        senha = scan.nextLine();
+
+        while (nomeUsuario.equalsIgnoreCase(senha)) {
+            System.out.println("A Senha não pode ser igual ao Nome de Usuário, digite novamente");
+            System.out.print("Senha: ");
+            senha = scan.nextLine();
+        }
+    }
+
+}
