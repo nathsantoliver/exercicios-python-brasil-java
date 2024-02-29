f537b5da769559e45de9d991d0efa756197685d2
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-02-28 21:01:52 -0300

    solucao da questao 09 e 10 - conversores de temperatura

---
+++ src/sequencial/Questao09.java
@@ -0,0 +1,17 @@
+package sequencial;
+
+import java.util.Scanner;
+
+public class Questao09 {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao conversor Fahrenheit para Celsius!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite a temperatura em Graus Fahrenheit: ");
+        double fr = scan.nextDouble();
+
+        double c = 5 * ((fr - 32) / 9);
+
+        System.out.printf("%.1fºF = ", fr);
+        System.out.printf("%.1fºC", c);
+    }
+}
