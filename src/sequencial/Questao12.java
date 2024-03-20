5c6d5566014be673d3f06443e0516d17b4f93363
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-03-20 15:15:03 -0300

    solucao da questao 12

---
+++ src/sequencial/Questao12.java
@@ -0,0 +1,19 @@
+package sequencial;
+
+import java.util.Scanner;
+
+public class Questao12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite a sua altura: ");
+        double altura = scan.nextDouble();
+
+        // calculo do peso ideal
+        double pesoIdeal = (72.7 * altura) - 58;
+
+        System.out.printf("O seu peso ideal é de %.2f", pesoIdeal);
+
+        scan.close();
+    }
+}
