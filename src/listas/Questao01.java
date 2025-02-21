3b49de68f8437630fee6547601da9ca356549234
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-20 21:30:53 -0300

    solving the first question of the List file

---
+++ src/listas/Questao01.java
@@ -0,0 +1,15 @@
+package listas;
+
+import java.util.Arrays;
+
+public class Questao01 {
+    public static void main(String[] args) {
+
+        int[] numeros = { 45, 23, 15, 84, 39 };
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
