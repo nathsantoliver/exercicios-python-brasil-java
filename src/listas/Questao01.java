f2a52a95ca6a359ced9ce924e55a987e12434a9a
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-03-13 21:31:32 -0300

    solving the first question of the List file

--- src/listas/Questao01.java
+++ src/listas/Questao01.java
@@ -1,14 +1,12 @@
 package listas;
 
-import java.util.Arrays;
-
 public class Questao01 {
     public static void main(String[] args) {
 
         int[] numeros = { 45, 23, 15, 84, 39 };
 
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);
+        for (int numero : numeros) {
+            System.out.println(numero);
         }
 
     }
