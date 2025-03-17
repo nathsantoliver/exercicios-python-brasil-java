24e743de2388ba7911399b61a1ea6260437dd0a4
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-03-17 20:24:46 -0300

    solving the second question of the List file

---
+++ src/listas/Questao02.java
@@ -0,0 +1,13 @@
+package listas;
+
+public class Questao02 {
+    public static void main(String[] args) {
+
+        double nums[] = { 45.80, 67.50, 83.80, 3.50, 15.20, 32.00, 92.00, 61.90, 27.70, 51.10 };
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(nums[i]);
+        }
+
+    }
+}
